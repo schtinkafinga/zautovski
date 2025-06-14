c3c72e340e717b4826edf478cf0e75a2ea828603
gio <giozautaa@gmail.com>
2025-06-15 03:04:34 +0400

    mobile

--- src/pages/home/components/hero/index.tsx
+++ src/pages/home/components/hero/index.tsx
@@ -9,13 +9,13 @@ import animationScroll from "@/assets/animations/Scroll-Animation - 174949592744
 
 const Hero: React.FC = () => {
   return (
-    <div className="hero flex relative w-full h-[100vh] items-center justify-between ">
-      <div className="hero-info autoBlur absolute left-[5%] ">
-        <div className="hero-info-title rounded-[50px] flex justify-center items-center text-[#727fde] py-[8px] px-[5px] border border-[#727fde77] w-[250px] bg-[#2200493d] shadow-[0_0_5px_#727fde88]">
+    <div className="hero flex     relative w-full h-[100vh]  ">
+      <div className="hero-info  sm:autoBlur  bottom-[2%] sm:bottom-[20%]  absolute left-[4%] sm:left-[5%]    ">
+        <div className="hero-info-title rounded-[50px]  flex justify-center items-center text-[#727fde] py-[8px] px-[5px] border border-[#727fde77] w-[250px] bg-[#2200493d] shadow-[0_0_5px_#727fde88]">
           <FontAwesomeIcon icon={faSketch} />
           Front-end Developer Portfolio
         </div>
-        <h1 className="text-[60px] max-w-[600px] font-[700] leading-[70px] mt-[40px] mb-[30px]">
+        <h1 className="text-[35px] sm:text-[60px]  max-w-[600px] font-[700] leading-[50px] sm:leading-[70px] mt-[20px] sm:mt-[40px] mb-[30px]">
           Providing <span className="gradient">the best</span> Project
           <span className="gradient"> Experience</span>
         </h1>
@@ -24,15 +24,15 @@ const Hero: React.FC = () => {
           Software development. Check out my skills.
         </p>
 
-        <button className="text-white flex items-center transition duration-300 justify-center gap-2 py-[15px] px-[35px] border rounded-[10px] bg-[#2200493d] border-[#727fdeb4] shadow-[0_0_5px_#727fde86] cursor-pointer hover:shadow-[0_0_15px_#727fde86] ">
+        <button className="text-white  flex items-center transition duration-300 justify-center gap-2 py-[15px] px-[35px] border rounded-[10px] bg-[#2200493d] border-[#727fdeb4] shadow-[0_0_5px_#727fde86] cursor-pointer hover:shadow-[0_0_15px_#727fde86] ">
           <SendHorizontal className="w-4 h-4 " />
           Contact Me
         </button>
       </div>
 
-      <div className="hero-vid-box  absolute right-[0%] ">
+      <div className="hero-vid-box bottom-[48%]   sm:bottom-[-15%] absolute right-[0%]  ">
         <video
-          className="autoBlur h-[900px] mix-blend-lighten"
+          className="sm:autoBlur max-h-[900px] mix-blend-lighten "
           autoPlay
           loop
           muted
@@ -41,7 +41,7 @@ const Hero: React.FC = () => {
         ></video>
       </div>
 
-      <div className="scroll-down w-[30px]  h-[50px] absolute left-[49%] bottom-[8%] rounded-[50px] cursor-pointer shadow-[0_0_15px_rgba(211,211,211,0.477)]  border-2 border-[lightgray]">
+      <div className="scroll-down w-[30px]   h-[50px] absolute left-[55%] sm:left-[49%] bottom-[20px]  sm:bottom-[8%] rounded-[50px] cursor-pointer shadow-[0_0_15px_rgba(211,211,211,0.477)]  border-2 border-[lightgray]">
         <Lottie
           animationData={animationScroll}
           loop={true}
