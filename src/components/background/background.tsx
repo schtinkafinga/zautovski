49e3bc23dcf5fae413187e0c4e08d145d84ee646
giorgi <zautashvili1994@gmail.com>
2025-06-17 21:00:37 +0400

    video

--- src/components/background/background.tsx
+++ src/components/background/background.tsx
@@ -3,16 +3,18 @@ import React from "react";
 const Background: React.FC = () => {
   return (
     <video
+      poster="/images/galaxy.png"
       loop
       autoPlay
       muted
       playsInline
+      preload="auto"
       src="/videos/galaxy2.mp4"
-      className="fixed z-[-1] w-[100%] right-0 bottom-0  object-cover "
+      className="fixed z-[-1] w-full right-0 bottom-0  object-cover "
     ></video>
   );
 };
 
 export default Background;
 
-//mix-blend-lighten  ეს შეგიძლია დაამატო ვიდეოზე თუგინდა რომ ფერი დაედოს ბექში 
+//mix-blend-lighten  ეს შეგიძლია დაამატო ვიდეოზე თუგინდა რომ ფერი დაედოს ბექში
