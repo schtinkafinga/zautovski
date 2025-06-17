e49576a5bd5e2823ce32da22b61400e303cbe5e1
giorgi <zautashvili1994@gmail.com>
2025-06-17 20:38:04 +0400

    test

--- src/components/background/background.tsx
+++ src/components/background/background.tsx
@@ -7,8 +7,8 @@ const Background: React.FC = () => {
       autoPlay
       muted
       playsInline
-      src="videos/galaxy2.mp4"
-      className="fixed right-0 bottom-0 w-full h-full object-cover z-[-1] mix-blend-lighten "
+      src="/videos/galaxy2.mp4"
+      className="fixed z-[-1] right-0 bottom-0 w-full h-full object-cover  "
     ></video>
   );
 };
